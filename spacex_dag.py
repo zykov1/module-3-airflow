fc5c855dcd80f803f67614e020595523c0810ca5
Egor Mateshuk <e.mateshuk@gmail.com>
2021-02-17 23:58:11 +0300

    fix

--- spacex_dag.py
+++ spacex_dag.py
@@ -18,13 +18,13 @@ dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
 t1 = BashOperator(
     task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /usr/local/airflow/data", 
+    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
     dag=dag
 )
 
 t2 = BashOperator(
     task_id="print_data", 
-    bash_command="cat /root/airflow/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
     params={"rocket": "all"}, # falcon1/falcon9/falconheavy
     dag=dag
 )
